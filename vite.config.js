d49091306179fbb247b0a7557d4bd9d64da571aa
Emmanuel Asiimwe <90935867+asiimwemmanuel@users.noreply.github.com>
2025-07-28 09:12:18 +0300

    enabling https

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-
+import fs from 'fs';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    port: 5173, // backend expects this on local dev
+    // need for cookies
+    https: {
+      key: fs.readFileSync('./certs/key.pem'),
+      cert: fs.readFileSync('./certs/cert.pem'),
+    },
+  },
 });
+
